ef21ee49199bc74ef75e72ff516415c778b7ebbe
Jonathan Kim <jonathankim980@gmail.com>
2023-02-09 15:50:19 -0600

    main page style complete

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { AiOutlineMenu } from "react-icons/ai";
+import { BiArrowFromRight } from "react-icons/bi";
 
 import { links } from "../data/importData";
 import { useStateContext } from "../contexts/ContextProvider";
@@ -19,23 +20,23 @@ const Sidebar = () => {
   };
 
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white text-md m-2";
   const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-white m-2";
 
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+    <div className="h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 bg-main-bg">
       {activeMenu && (
         <>
-          <div className="flex justify-between items-center">
+          <div className="flex flex-row justify-between items-center p-3 hover:opacity-80">
             <Link
               to="/"
               onClick={handleCloseSideBar}
-              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+              className="items-center gap-3 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Bug Tracker</span>
+              <SiShopware /> <p>Bug Tracker</p>
             </Link>
-            <TooltipComponent content="Menu" position="BottomCenter">
+            <TooltipComponent content="collapse" position="BottomCenter">
               <button
                 type="button"
                 onClick={() => setActiveMenu(!activeMenu)}
@@ -44,8 +45,12 @@ const Sidebar = () => {
               >
                 <MdOutlineCancel />
               </button>
-              <button onClick={() => setActiveMenu(false)} color={currentColor}>
-                <AiOutlineMenu />
+              <button
+                classname=" hover:opacity-80"
+                onClick={() => setActiveMenu(false)}
+                color={currentColor}
+              >
+                <BiArrowFromRight size={30} />
               </button>
             </TooltipComponent>
           </div>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -47,33 +47,33 @@ const Main = () => {
   const [data, setData] = useState([]);
   const { user } = useAuth();
   return (
-    <div className="flex flex-col h-full pr-4 bg-slate-500">
-      <div className="flex flex-row justify-center w-full bg-sky-400">
+    <div className="flex flex-col h-full px-4">
+      <div className="flex flex-row justify-center w-full pb-5">
         <h3>Welcome, {user.displayName}</h3>
       </div>
-      <div className="flex flex-row justify-between w-full py-4 bg-sky-500">
-        <div className="flex flex-col justify-center items-center w-1/4 mr-4 py-12 px-12 rounded-2xl bg-slate-500">
+      <div className="flex flex-row justify-between w-full py-4">
+        <div className="flex flex-col justify-center items-center w-1/4 mr-4 py-12 px-12 rounded-2xl bg-blue-600 text-white font-bold">
           <h1>Active Projects</h1>
           <h2>15</h2>
         </div>
-        <div className="flex flex-col justify-center items-center w-1/4 mx-4 py-12 px-12 rounded-2xl bg-slate-500">
+        <div className="flex flex-col justify-center items-center w-1/4 mx-4 py-12 px-12 rounded-2xl bg-yellow-400 text-white font-bold">
           <h1>Total Tickets</h1>
           <h2>15</h2>
         </div>
-        <div className="flex flex-col justify-center items-center w-1/4 mx-4 py-12 px-12 rounded-2xl bg-slate-500">
+        <div className="flex flex-col justify-center items-center w-1/4 mx-4 py-12 px-12 rounded-2xl bg-green-600 text-white font-bold">
           <h1>Unassigned Tickets</h1>
           <h2>15</h2>
         </div>
-        <div className="flex flex-col justify-center items-center w-1/4 ml-4 py-12 px-12 rounded-2xl bg-slate-500">
+        <div className="flex flex-col justify-center items-center w-1/4 ml-4 py-12 px-12 rounded-2xl bg-red-600 text-white font-bold">
           <h1>Analytics</h1>
           <h2>15</h2>
         </div>
       </div>
 
-      <div className="flex flex-row justify-between h-fit w-full py-4 bg-sky-600">
+      <div className="flex flex-row justify-between h-fit w-full py-4">
         <div className="flex flex-col justify-center items-center w-1/4 mr-4 px-3 rounded-2xl bg-white">
-          <div className="flex flex-row items-center w-full h-1/4 my-1 border-b-4 border-indigo-500 ">
-            <div className="flex justify-center roundex 2xl p-4 mr-2 border-2 border-indigo-300 rounded-2xl">
+          <div className="flex flex-row items-center w-full h-1/4 my-1 border-b-2 border-slate-300 ">
+            <div className="flex justify-center roundex 2xl p-4 mr-2 border-1 border-slate-300 rounded-xl">
               <FaUserAlt />
             </div>
             <div className="flex flex-col">
@@ -81,8 +81,8 @@ const Main = () => {
               <p>10</p>
             </div>
           </div>
-          <div className="flex flex-row items-center w-full h-1/4 mb-1 border-b-4 border-indigo-500">
-            <div className="flex justify-center roundex 2xl p-4 mr-2 border-2 border-indigo-300 rounded-2xl">
+          <div className="flex flex-row items-center w-full h-1/4 mb-1 border-b-2 border-slate-300">
+            <div className="flex justify-center roundex 2xl p-4 mr-2 border-1 border-slate-300 rounded-xl">
               <FaUsers />
             </div>
             <div className="flex flex-col">
@@ -90,8 +90,8 @@ const Main = () => {
               <p>10</p>
             </div>
           </div>
-          <div className="flex flex-row items-center w-full h-1/4 mb-1 border-b-4 border-indigo-500">
-            <div className="flex justify-center roundex 2xl p-4 mr-2 border-2 border-indigo-300 rounded-2xl">
+          <div className="flex flex-row items-center w-full h-1/4 mb-1 border-b-2 border-slate-300">
+            <div className="flex justify-center roundex 2xl p-4 mr-2 border-1 border-slate-300 rounded-xl">
               <AiFillBug />
             </div>
             <div className="flex flex-col">
@@ -100,7 +100,7 @@ const Main = () => {
             </div>
           </div>
           <div className="flex flex-row items-center w-full h-1/4 mb-1">
-            <div className="flex justify-center roundex 2xl p-4 mr-2 border-2 border-indigo-300 rounded-2xl">
+            <div className="flex justify-center roundex 2xl p-4 mr-2 border-1 border-slate-300 rounded-xl">
               <GrUserWorker />
             </div>
             <div className="flex flex-col">
@@ -115,35 +115,35 @@ const Main = () => {
             Company Data
           </div>
           <div className="flex flex-col justify-center items-center h-4/5 w-full">
-            <div className="flex flex-row justify-between items-center w-full h-1/4 px-4 mb-1 border-b-4 border-indigo-500">
+            <div className="flex flex-row justify-between items-center w-full h-1/4 px-4 mb-1 border-b-2 border-slate-300">
               <div className="flex flex-row justify-center items-center w-1/5 mr-3">
                 <FaUsers size={30} />
               </div>
               <div className="flex flex-row justify-between items-center w-4/5 h-full">
-                <p>Members</p>
-                <p className="flex flex-row justify-center items-center w-10 p-1 rounded-md border-4 border-indigo-500">
+                <p class="text-teal-400 font-semibold">Members</p>
+                <p className="flex flex-row justify-center items-center w-10 p-1 rounded-md border-2 border-slate-300">
                   27
                 </p>
               </div>
             </div>
-            <div className="flex flex-row items-center w-full h-1/4 px-4 mb-1 border-b-4 border-indigo-500">
+            <div className="flex flex-row items-center w-full h-1/4 px-4 mb-1 border-b-2 border-slate-300">
               <div className="flex flex-row justify-center items-center w-1/5 mr-3">
                 <AiFillFolder size={30} />
               </div>
               <div className="flex flex-row justify-between items-center w-4/5 h-full">
-                <p>Projects</p>
-                <p className="flex flex-row justify-center items-center w-10 p-1 rounded-md border-4 border-indigo-500">
+                <p class="text-teal-400 font-semibold">Projects</p>
+                <p className="flex flex-row justify-center items-center w-10 p-1 rounded-md border-2 border-slate-300">
                   4
                 </p>
               </div>
             </div>
-            <div className="flex flex-row justify-between items-center w-full h-1/4 px-4 mb-1 border-b-4 border-indigo-500">
+            <div className="flex flex-row justify-between items-center w-full h-1/4 px-4 mb-1 border-b-2 border-slate-300">
               <div className="flex flex-row justify-center items-center w-1/5 mr-3">
                 <GiTicket size={30} />
               </div>
               <div className="flex flex-row justify-between items-center w-4/5 h-full">
-                <p>Tickets</p>
-                <p className="flex flex-row justify-center items-center w-10 p-1 rounded-md border-4 border-indigo-500">
+                <p class="text-teal-400 font-semibold">Tickets</p>
+                <p className="flex flex-row justify-center items-center w-10 p-1 rounded-md border-2 border-slate-300">
                   100
                 </p>
               </div>
@@ -153,8 +153,8 @@ const Main = () => {
                 <IoMdNotifications size={30} />
               </div>
               <div className="flex flex-row justify-between items-center w-4/5 h-full">
-                <p>Notifications</p>
-                <p className="flex flex-row justify-center items-center w-10 p-1 rounded-md border-4 border-indigo-500">
+                <p class="text-teal-400 font-semibold">Notifications</p>
+                <p className="flex flex-row justify-center items-center w-10 p-1 rounded-md border-2 border-slate-300">
                   1
                 </p>
               </div>
@@ -217,8 +217,8 @@ const Main = () => {
           </GridComponent>
         </div> */}
       </div>
-      <div className="flex flex-row justify-center items-center h-1/3 rounded-2xl bg-sky-700">
-        <div className="flex justify-center items-center w-1/3 mr-4 rounded-2xl bg-sky-800">
+      <div className="flex flex-row justify-center items-center h-1/3 rounded-2xl">
+        <div className="flex justify-center items-center w-1/3 mr-4 rounded-2xl bg-white">
           <PieChart
             id="pie-chart"
             data={pieChartData}
@@ -228,7 +228,7 @@ const Main = () => {
             background="transparent"
           />
         </div>
-        <div className="flex justify-center items-center w-1/3 mx-4 rounded-2xl bg-sky-800">
+        <div className="flex justify-center items-center w-1/3 mx-4 rounded-2xl bg-white">
           <PieChart
             id="pie-chart2"
             data={pieChartData2}
@@ -238,7 +238,7 @@ const Main = () => {
             background="transparent"
           />
         </div>
-        <div className="flex justify-center items-center w-1/3 ml-4 rounded-2xl bg-sky-800">
+        <div className="flex justify-center items-center w-1/3 ml-4 rounded-2xl bg-white">
           <PieChart
             id="pie-chart3"
             data={pieChartData2}
